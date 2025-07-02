52b92403d9d6f4ff5d252a628275205d22b5ffaa
Mason Myre <masonmyre@gmail.com>
2025-07-01 23:43:46 -0500

    Update for Elmar SOlar MPPTs

--- Array-Screen-7-02-2024.py
+++ Array-Screen-7-02-2024.py
@@ -1,7 +1,8 @@
 '''
-Last Edit: 07/03/2024
+Last Edit: 07/01/2025
+Updated CAN addresses for multiple offset trackers
 
-Edits:
+07/03/2024 Edits:
 Made the total array wattage look a little nicer
 Added comments
 
@@ -26,22 +27,10 @@ import terminalio
 import adafruit_ssd1325
 from adafruit_mcp2515       import MCP2515 as CAN
 from adafruit_mcp2515.canio import RemoteTransmissionRequest, Message, Match, Timer
-from adafruit_display_text import label
+from adafruit_display_text  import label
 import adafruit_mcp2515
-import microcontroller 
+import microcontroller
 
-
-"""
-======================================================================================================================================
-MPPT CANBus Codes
------------------
-Message ID | Value 1            | Value 2            | Value 3         | Value 4     |
---------------------------------------------------------------------------------------
-0x600      | Subarray 1 Voltage | Subarray 1 Current | Battery Voltage | MPPT 1 Temp |
-0x601      | Subarray 2 Voltage | Subarray 2 Current | Battery Voltage | MPPT 1 Temp |
-0x602      | Subarray 3 Voltage | Subarray 3 Current | Battery Voltage | MPPT 2 Temp |
-======================================================================================================================================
-"""
 
 # Release the displays and start the clock
 boot_time = time.monotonic()
@@ -72,7 +61,6 @@ display = adafruit_ssd1325.SSD1325(display_bus, width=WIDTH, height=HEIGHT)
 display.brightness = 1.0
 
 
-
 startTime = time.time()
 # Make the display context
 splash = displayio.Group()
@@ -80,7 +68,7 @@ display.root_group = splash
 
 color_bitmap = displayio.Bitmap(display.width, display.height, 1)
 color_palette = displayio.Palette(1)
-color_palette[0] =0x000000  # Black
+color_palette[0] = 0x000000  # Black
 
 bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
 splash.append(bg_sprite)
@@ -100,39 +88,85 @@ time.sleep(2.5)
 splash.pop(-1) #since this was just the startup stuff we never use it again and pop it out of the splash
 
 
-
 #variable initialization
-subArr1V = subArr1I = subArr2V = subArr2I = subArr3V = subArr3I = mppttemp1 = mppttemp2 = -1
+class Tracker():
+    '''
+    Elmar Solar Race MPPTs
+    https://docs.prohelion.com/MPPTs/pdfs/Elmar%20Solar%20MPPT%20Race%202021.pdf
+    '''
+    def __init__(self):
+        self.v_in = -1.0
+        self.i_in = -1.0
+        self.v_out = -1.0
+        self.i_out = -1.0
+        self.w = 0
+        self.fet_temp = -1.0
+        self.control_temp = -1.0
 
-subArr1W = subArr2W = subArr3W = totalWatt = sendtime = 0
+    def process600(self, data):
+        '''
+        Field, Message Bytes, Data Type, Units
+        Input Voltage, 0-3, Float, Volt
+        Input Current, 4-7, Float, Ampere
+        '''
+        holder = struct.unpack('<ff', next_message.data)
+        self.v_in = holder[0]
+        self.i_in = holder[1]
+
+    def process601(self, data):
+        '''
+        Field, Message Bytes, Data Type, Units
+        Output Voltage, 0-3, Float, Volt
+        Output Current, 4-7, Float, Ampere
+        '''
+        holder = struct.unpack('<ff', next_message.data)
+        self.v_out = holder[0]
+        self.i_out = holder[1]
+        self.w = self.v_out * self.i_out
+
+    def process602(self, data):
+        '''
+        Field, Message Bytes, Data Type, Units
+        Mosfet Temperature, 0-3, Float, Celcius
+        Controller Temperature, 4-7, Float, Celcius
+        '''
+        holder = struct.unpack('<ff', next_message.data)
+        self.fet_temp = holder[0]
+        self.controller_temp = holder[1]
+
+tracker1 = Tracker()
+tracker2 = Tracker()
+tracker3 = Tracker()
+
+totalWatt = send_time = 0
 
 
 # two different functions for initialization of the screen and updating 
 def initScreen():
     # Draw labels for each of the arrays
     text_group = displayio.Group(scale=1, x=2, y=8)
-    text = " 1       2       3"
+    text = "   1      2      3"
     text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF)
     text_group.append(text_area)  # Subgroup for text scaling
     splash.append(text_group)
     
     #write in the data coming from the subarrays
     text_group = displayio.Group(scale=1, x=2, y=20)
-    text = "{:04.1f}    {:04.1f}    {:04.1f}".format(subArr1W, subArr2W, subArr3W)
+    text = "W:{:04.1f}   {:04.1f}   {:04.1f}".format(tracker1.w, tracker2.w, tracker3.w)
     text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF)
     text_group.append(text_area)  # Subgroup for text scaling
     splash.append(text_group)
     
     #write in the total wattage we are getting from the array
     text_group = displayio.Group(scale=2, x=2, y=40)
-    text = "  T: {:04.1f}".format(subArr1W + subArr2W + subArr3W)
+    text = "  T:{:04.1f}".format(tracker1.w + tracker2.w + tracker3.w)
     text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF)
     text_group.append(text_area)  # Subgroup for text scaling
     splash.append(text_group)
 
     #display the temperatures we are getting from the power trackers
-    text_group = displayio.Group(scale=1, x=10, y=60)
-    text = "T1: {:04.1f}  T2: {:04.1f}".format(mppttemp1,mppttemp2)
+    text_group = displayio.Group(scale=1, x=2, y=60)
+    text = "C:{:3.0f}    {:3.0f}    {:3.0f}".format(tracker1.fet_temp, tracker2.fet_temp, tracker3.fet_temp)
     text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF)
     text_group.append(text_area)  # Subgroup for text scaling
     splash.append(text_group)
@@ -148,23 +182,23 @@ def drawScreen():
     splash[-4] = text_group
     '''
 
-    # update subarray wattage
+    #write in the data coming from the subarrays
     text_group = displayio.Group(scale=1, x=2, y=20)
-    text = "{:04.1f}    {:04.1f}    {:04.1f}".format(subArr1W, subArr2W, subArr3W)
+    text = "W:{:04.1f}   {:04.1f}   {:04.1f}".format(tracker1.w, tracker2.w, tracker3.w)
     text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF)
     text_group.append(text_area)  # Subgroup for text scaling
     splash[-3] = text_group
-
-    # update total array wattage
+    
+    #write in the total wattage we are getting from the array
     text_group = displayio.Group(scale=2, x=2, y=40)
-    text = "  T: {:04.1f}".format(subArr1W + subArr2W + subArr3W)
+    text = "  T:{:04.1f}".format(tracker1.w + tracker2.w + tracker3.w)
     text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF)
     text_group.append(text_area)  # Subgroup for text scaling
     splash[-2] = text_group
 
-    # update power tracker temperatures
-    text_group = displayio.Group(scale=1, x=10, y=60)
-    text = "T1: {:04.1f}  T2: {:04.1f}".format(mppttemp1,mppttemp2)
+    #display the temperatures we are getting from the power trackers
+    text_group = displayio.Group(scale=1, x=2, y=60)
+    text = "C:{:3.0f}    {:3.0f}    {:3.0f}".format(tracker1.fet_temp, tracker2.fet_temp, tracker3.fet_temp)
     text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF)
     text_group.append(text_area)  # Subgroup for text scaling
     splash[-1] = text_group
@@ -182,76 +216,46 @@ time.sleep(0.2)
 runTime = time.time()
 
 while True:
-
     #get the CAN data
     with mcp.listen(timeout=0) as listener:
-        
         #send data to portenta through the uart line
-        if(time.time()-sendtime>1):
-            uart.write(struct.pack('<ffffff',subArr1W,subArr2W,subArr3W,totalWatt,mppttemp1,mppttemp2))
-            sendtime=time.time()
+        current_time = time.time()
+        time_since_send = send_time - current_time
+        if time_since_send > 1:
+            # TODO: I don't know what the protocol is here to throw a 3rd temp in
+            uart.write(struct.pack('<ffffff', tracker1.w, tracker2.w, tracker3.w, totalWatt, tracker1.fet_temp, tracker2.fet_temp))
+            send_time=time.time()
 
-        totalWatt = subArr1W+subArr2W+subArr3W #calculate total wattage
+        totalWatt = tracker1.w + tracker2.w + tracker3.w #calculate total wattage
 
         _shaune_theCAN_isfull()
         
         drawScreen() #update the screen with CAN data
         #Here starts where we do the CAN things
         message_count = listener.in_waiting()
-        print("message count = {}".format(message_count),end = '\n')
+        print("message count = {}".format(message_count), end = '\n')
         if message_count == 0:
-
             continue
-        
         next_message = listener.receive()
-        message_num = 0
-
         
-        while not next_message is None:
-            drawScreen()
-            message_num += 1
-
+        while next_message is not None:
             # Check the id to properly unpack it
-            if next_message.id == 0x602:
-
-            #unpack and print the message
-                holder = struct.unpack('<hhhh',next_message.data)
-                subArr1V = holder[0]/100
-                subArr1I = holder[1]/1000
-                subArr1W = subArr1V*subArr1I
-                
-                #print("Message From: {}: [V = {}; A = {}]".format(hex(next_message.id),voltage,current))
-
-
-
-            if next_message.id == 0x603:
-
-            #unpack and print the message
-                holder = struct.unpack('<hhhh',next_message.data)
-                subArr2V = holder[0]/100
-                subArr2I = holder[1]/1000
-                subArr2W = subArr2V*subArr2I
-                mppttemp1 = ((holder[3]/100)*9/5)+32
-                
-                #print("Message From: {}: [V = {}; A = {}]".format(hex(next_message.id),voltage,current))
-
-
-            if next_message.id == 0x604:
-
-            #unpack and print the message
-                holder = struct.unpack('<hhhh',next_message.data)
-                subArr3V = holder[0]/100
-                subArr3I = holder[1]/1000
-                subArr3W = subArr3V*subArr3I
-                mppttemp2 = ((holder[3]/100)*9/5)+32
-                
-                #print("Message From: {}: [V = {}; A = {}]".format(hex(next_message.id),voltage,current))
-
-
+            if next_message.id == 0x610:
+                tracker1.process600(next_message.data)
+            elif next_message.id == 0x611:
+                tracker1.process601(next_message.data)
+            elif next_message.id == 0x612:
+                tracker1.process602(next_message.data)
+            elif next_message.id == 0x620:
+                tracker2.process600(next_message.data)
+            elif next_message.id == 0x621:
+                tracker2.process601(next_message.data)
+            elif next_message.id == 0x622:
+                tracker2.process602(next_message.data)
+            elif next_message.id == 0x630:
+                tracker3.process600(next_message.data)
+            elif next_message.id == 0x631:
+                tracker3.process601(next_message.data)
+            elif next_message.id == 0x632:
+                tracker3.process602(next_message.data)
             next_message = listener.receive()            
-
-
-
-    
-     
-
